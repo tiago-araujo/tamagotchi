632ca6cfff19a5ccc93bea39397d22af24eb861b
Tiago Araujo <tiaguzaum_@hotmail.com>
2015-06-04 00:16:33 -0400

    nome e metodo dos botões criados

--- tamagotchi.cpp
+++ tamagotchi.cpp
@@ -1,5 +1,6 @@
 #include "tamagotchi.h"
 #include "ui_tamagotchi.h"
+#include "qgraphicsscene.h"
 
 Tamagotchi::Tamagotchi(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,34 @@ Tamagotchi::Tamagotchi(QWidget *parent) :
 Tamagotchi::~Tamagotchi()
 {
     delete ui;
+}
+
+
+
+void Tamagotchi::on_interagirButton_clicked()
+{
+    QGraphicsScene inter = new QGraphicsScene(ui->telaTama);
+    inter.setSceneRect(ui->telaTama->rect());
+    ui->telaTama->setScene(inter);
+}
+
+void Tamagotchi::on_cantarButton_clicked()
+{
+    QGraphicsScene cantar = new QGraphicsScene(ui->telaTama);
+    cantar.setSceneRect(ui->telaTama->rect());
+    ui->telaTama->setScene(cantar);
+}
+
+void Tamagotchi::on_alimentarButton_clicked()
+{
+    QGraphicsScene alimentar = new QGraphicsScene(ui->telaTama);
+    alimentar.setSceneRect(ui->telaTama->rect());
+    ui->telaTama->setScene(alimentar);
+}
+
+void Tamagotchi::on_dormirButton_clicked()
+{
+    QGraphicsScene dormir = new QGraphicsScene(ui->telaTama);
+    dormir.setSceneRect(ui->telaTama->rect());
+    ui->telaTama->setScene(dormir);
 }
--- tamagotchi.h
+++ tamagotchi.h
@@ -15,6 +15,17 @@ public:
     explicit Tamagotchi(QWidget *parent = 0);
     ~Tamagotchi();
 
+private slots:
+    void on_pushButton_clicked();
+
+    void on_interagirButton_clicked();
+
+    void on_cantarButton_clicked();
+
+    void on_alimentarButton_clicked();
+
+    void on_dormirButton_clicked();
+
 private:
     Ui::Tamagotchi *ui;
 };
